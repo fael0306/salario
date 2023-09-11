7d27c26b0a0c107a1a18aaaaa7e904607254590a
Manteiga <rafael.manteiga@hotmail.com>
2023-09-11 03:53:53 -0300

    Add files via upload

--- salario.py
+++ salario.py
@@ -2,14 +2,15 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 salarios = pd.read_csv("C:\\Users\\f0fp1107\\Downloads\\salarionominalnecessario.csv", sep=",", encoding="latin_1")
-print(salarios.head())
+#print(salarios.head())
 
 salarios['perc'] = salarios['Salário mínimo nominal']/salarios['Salário mínimo necessário']
-print(salarios.head())
+#print(salarios.head())
 
 salarios = salarios.iloc[::-1]
 salarios = salarios[len(salarios)-12:]
-datas = pd.date_range(start='2022-09',end='2023-08',periods=12)
+
+datas = pd.date_range(start='2022-09',end='2023-08',freq='MS')
 datas = datas.strftime('%m/%Y')
 
 plt.plot(datas,salarios['perc'])
